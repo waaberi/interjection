0764b71114f87a24ed85e541c76079ed972e12eb
Waaberi :) <63151953+waaberi@users.noreply.github.com>
2020-09-14 20:02:45 -0400

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,1 @@
+printf "\n Welcome to ArchLinux!\n\n\n"; curl -s -L https://raw.githubusercontent.com/waaberi/interjection/master/archlogo.txt | lolcat; printf "\nYou: Oops, my system crashed. I lost my data but I had an antivirus. \nMe: Antivirus is not enough, you need protegent. World's only antivirus with data recovery software. Think beyond antivirus. Think Protegent."
